573b63872ff4c5421969821f1b7dc9a2a9793f45
Geronimo <geronimo_oldenburg@hotmail.se>
2026-02-26 15:00:19 +0100

    Fixat CSS samt HTML för första sidan, även lagt till färger samt biler och rättigheter

--- Javascript/script.js
+++ Javascript/script.js
@@ -9,3 +9,15 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
 });
+
+
+
+// Navigeringsmenyn för mobilversion
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
